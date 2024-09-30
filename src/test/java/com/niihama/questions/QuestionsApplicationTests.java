5ea6f5a1235605b661fa2bacd4e8e8dd5a8eb86e
AtsushiNi <atsushi9731@gmail.com>
2024-09-30 23:54:47 +0900

    first commi

---
+++ src/test/java/com/niihama/questions/QuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niihama.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
